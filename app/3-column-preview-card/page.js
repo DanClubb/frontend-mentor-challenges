abb0776fda540bd8d1a3023ba87a360bb948af67
daniel-clubb <danclubb004@gmail.com>
2023-08-26 13:58:55 +0200

    completed 3 column preview card challenge

--- app/3-column-preview-card/page.js
+++ app/3-column-preview-card/page.js
@@ -2,54 +2,58 @@ import Image from "next/image";
 import sedans from "./images/icon-sedans.svg";
 import suvs from "./images/icon-suvs.svg";
 import luxury from "./images/icon-luxury.svg";
-import "./3-column-preview-card.module.css";
+import styles from "./3-column-preview-card.module.css";
+import { Lexend_Deca } from "next/font/google";
+import { Big_Shoulders_Display } from "next/font/google";
+
+const lexend_deca = Lexend_Deca({ weight: "400", subsets: ["latin"] });
+const big_shoulders_display = Big_Shoulders_Display({
+  weight: "700",
+  subsets: ["latin"],
+});
 
 export default function ThreeColumnPreviewCard() {
   return (
-    <main>
-      <div className="column-preview-card">
-        <section className="car sedans">
+    <main className={`${lexend_deca.className} ${styles.main}`}>
+      <div className={styles.columnPreviewCard}>
+        <section className={`${styles.column} ${styles.sedans}`}>
           <Image src={sedans} alt="sedan" />
-          <h1 className="column-title">SEDANS</h1>
+          <h1 className={big_shoulders_display.className}>SEDANS</h1>
           <p>
             Choose a sedan for its affordability and excellent fuel economy.
             Ideal for cruising in the city or on your next road trip.
           </p>
-          <button className="btn btn--sedans">Learn More</button>
+          <button className={`${styles.btn} ${styles.btnSedans}`}>
+            Learn More
+          </button>
         </section>
 
-        <section className="car suvs">
+        <section className={`${styles.column} ${styles.suvs}`}>
           <Image src={suvs} alt="SUV" />
 
-          <h1 className="column-title">SUVS</h1>
+          <h1 className={big_shoulders_display.className}>SUVS</h1>
           <p>
             Take an SUV for its spacious interior, power, and versatility.
             Perfect for your next family vacation and off-road adventures.
           </p>
 
-          <button className="btn btn--suvs">Learn More</button>
+          <button className={`${styles.btn} ${styles.btnSuvs}`}>
+            Learn More
+          </button>
         </section>
 
-        <section className="car luxury">
+        <section className={`${styles.column} ${styles.luxury}`}>
           <Image src={luxury} alt="luxury" />
 
-          <h1 className="column-title">LUXURY</h1>
+          <h1 className={big_shoulders_display.className}>LUXURY</h1>
           <p>
             Cruise in the best car brands without the bloated prices. Enjoy the
             enhanced comfort of a luxury rental and arrive in style.
           </p>
-          <button className="btn btn--luxury">Learn More</button>
+          <button className={`${styles.btn} ${styles.btnLuxury}`}>
+            Learn More
+          </button>
         </section>
-      </div>
-      <div class="attribution">
-        Challenge by{" "}
-        <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
-          Frontend Mentor{" "}
-        </a>
-        | Coded by{" "}
-        <a href="https://danclubb.vercel.app" target="_blank">
-          Daniel Clubb
-        </a>
       </div>
     </main>
   );
--- app/page.js
+++ app/page.js
@@ -23,6 +23,17 @@ export default function Home() {
           Product Preview Card
         </Link>
       </section>
+      <div className="attribution">
+        Challenge by{" "}
+        <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+          Frontend Mentor{" "}
+        </a>
+        | Coded by{" "}
+        <a href="https://danclubb.vercel.app" target="_blank">
+          Daniel Clubb
+        </a>
+        .
+      </div>
     </main>
   );
 }
