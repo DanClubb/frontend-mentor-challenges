de1d4ad46d4c0bf00cdd1ce717e25e691f771d9d
daniel-clubb <danclubb004@gmail.com>
2023-09-06 13:12:19 +0100

    neatend github link with logo, added skills practiced, add difficulty rating

--- app/components/ChallengeCard.js
+++ app/components/ChallengeCard.js
@@ -1,17 +1,43 @@
 import Image from "next/image";
 import Link from "next/link";
 import styles from "./styles/challenge-card.module.css";
+import github from "../../public/github-mark.png";
 
-function ChallengeCard({ route, img, title, description }) {
+function ChallengeCard({
+  route,
+  img,
+  title,
+  skills,
+  difficulty,
+  difficultyNumber,
+  description,
+}) {
   return (
     <Link href={route} className={styles.challengeCard}>
       <Image src={img} alt={`${title} preview`} />
       <div className={styles.content}>
         <h2 className={styles.cardTitle}>{title}</h2>
+        <div className={styles.challengeInfo}>
+          <div>
+            {skills.map((skill) => (
+              <span key={skill} className={`${styles[skill]} ${styles.skill}`}>
+                {skill}
+              </span>
+            ))}
+          </div>
+          <div className={`${styles.difficulty} ${styles[difficulty]}`}>
+            <div className={styles.difficultyNumber}>
+              <span>{difficultyNumber}</span>
+            </div>
+            <div>{difficulty}</div>
+          </div>
+        </div>
         <p>{description}</p>
-        <div>
+        <div className={styles.linkText}>
+          View project code in
           <Link href={route} className={styles.link}>
             GitHub
+            <Image src={github} width={25} height={25} alt="github logo" />
           </Link>
         </div>
       </div>
--- app/page.js
+++ app/page.js
@@ -1,37 +1,7 @@
 import ChallengeCard from "./components/ChallengeCard";
+import challenges from "./challenges.json";
 import CHALLENGE_PREVIEWS from "@/public/challenge-previews/challengePreviews";
 import styles from "./homepage.module.css";
-
-const challenges = {
-  resultsSummary: {
-    route: "/results-summary",
-    img: CHALLENGE_PREVIEWS.resultsSummary,
-    title: "Results Summary",
-    description:
-      "This news homepage will be an excellent opportunity to practice your CSS Grid skills. There will be lots of tricky decisions to make and plenty of learning opportunities!",
-  },
-  productPreviewCard: {
-    route: "/product-preview-card",
-    img: CHALLENGE_PREVIEWS.productPreviewCard,
-    title: "Product Preview Card",
-    description:
-      "This news homepage will be an excellent opportunity to practice your CSS Grid skills. There will be lots of tricky decisions to make and plenty of learning opportunities!",
-  },
-  newsHomepage: {
-    route: "/news-homepage",
-    img: CHALLENGE_PREVIEWS.newsHomepage,
-    title: "News Hompage",
-    description:
-      "This news homepage will be an excellent opportunity to practice your CSS Grid skills. There will be lots of tricky decisions to make and plenty of learning opportunities!",
-  },
-  columnPreviewCard: {
-    route: "/3-column-preview-card",
-    img: CHALLENGE_PREVIEWS.columnPreviewCard,
-    title: "3 Column Preview Card",
-    description:
-      "This news homepage will be an excellent opportunity to practice your CSS Grid skills. There will be lots of tricky decisions to make and plenty of learning opportunities!",
-  },
-};
 
 export default function Home() {
   return (
@@ -44,8 +14,11 @@ export default function Home() {
         {Object.keys(challenges).map((challenge) => (
           <ChallengeCard
             route={challenges[challenge].route}
-            img={challenges[challenge].img}
+            img={CHALLENGE_PREVIEWS[challenge]}
             title={challenges[challenge].title}
+            skills={challenges[challenge].skills}
+            difficulty={challenges[challenge].difficulty}
+            difficultyNumber={challenges[challenge].difficultyNumber}
             description={challenges[challenge].description}
             key={challenge}
           />
