7d0f9b505a7647a76a452a8abe0adfe6d35a90e1
daniel-clubb <danclubb004@gmail.com>
2023-09-06 17:06:14 +0100

    fixed github links, started sunnyside landing page

--- app/components/ChallengeCard.js
+++ app/components/ChallengeCard.js
@@ -13,35 +13,44 @@ function ChallengeCard({
   description,
 }) {
   return (
-    <Link href={route} className={styles.challengeCard}>
-      <Image src={img} alt={`${title} preview`} />
-      <div className={styles.content}>
-        <h2 className={styles.cardTitle}>{title}</h2>
-        <div className={styles.challengeInfo}>
-          <div>
-            {skills.map((skill) => (
-              <span key={skill} className={`${styles[skill]} ${styles.skill}`}>
-                {skill}
-              </span>
-            ))}
-          </div>
-          <div className={`${styles.difficulty} ${styles[difficulty]}`}>
-            <div className={styles.difficultyNumber}>
-              <span>{difficultyNumber}</span>
+    <div className={styles.challengeCard}>
+      <Link href={route}>
+        <Image src={img} alt={`${title} preview`} />
+        <div className={styles.content}>
+          <h2 className={styles.cardTitle}>{title}</h2>
+          <div className={styles.challengeInfo}>
+            <div>
+              {skills.map((skill) => (
+                <span
+                  key={skill}
+                  className={`${styles[skill]} ${styles.skill}`}
+                >
+                  {skill}
+                </span>
+              ))}
             </div>
-            <div>{difficulty}</div>
+            <div className={`${styles.difficulty} ${styles[difficulty]}`}>
+              <div className={styles.difficultyNumber}>
+                <span>{difficultyNumber}</span>
+              </div>
+              <div>{difficulty}</div>
+            </div>
           </div>
+          <p>{description}</p>
         </div>
-        <p>{description}</p>
-        <div className={styles.linkText}>
-          View project code in
-          <Link href={route} className={styles.link}>
-            GitHub
-            <Image src={github} width={25} height={25} alt="github logo" />
-          </Link>
-        </div>
+      </Link>
+      <div className={styles.linkText}>
+        View project code in
+        <a
+          href={`https://github.com/DanClubb/frontend-mentor-challenges/tree/main/app${route}`}
+          target="_blank"
+          className={styles.link}
+        >
+          GitHub
+          <Image src={github} width={25} height={25} alt="github logo" />
+        </a>
       </div>
-    </Link>
+    </div>
   );
 }
 
--- public/challenge-previews/challengePreviews.js
+++ public/challenge-previews/challengePreviews.js
@@ -3,6 +3,8 @@ const CHALLENGE_PREVIEWS = {
   columnPreviewCard: require("./column-preview-card.jpg").default,
   newsHomepage: require("./news-homepage.jpg").default,
   productPreviewCard: require("./product-preview-card.jpg").default,
+  sunnysideAgencyLandingPage: require("./sunnyside-landing-page-preview.jpg")
+    .default,
 };
 
 export default CHALLENGE_PREVIEWS;
