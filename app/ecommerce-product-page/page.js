5c8277dadeded213ee496cab09386174c339808a
daniel-clubb <danclubb004@gmail.com>
2023-09-23 14:08:07 +0100

    changed add to cart logic

--- app/ecommerce-product-page/page.js
+++ app/ecommerce-product-page/page.js
@@ -13,6 +13,7 @@ const kumbhSans = Kumbh_Sans({
 });
 
 export default function EcommerceProductPage() {
+  const [numItemsSelected, setNumItemsSelected] = useState(0);
   const [numItemsInCart, setNumItemsInCart] = useState(0);
   const [mobileNavExpanded, setMobileNavExpanded] = useState(false);
   const [cartIsVisible, setCartIsVisible] = useState(false);
@@ -33,11 +34,18 @@ export default function EcommerceProductPage() {
     setNumItemsInCart(0);
   };
 
-  const handleCartAddition = (amount) => {
-    setNumItemsInCart((prev) => {
+  const handleItemIncrement = (amount) => {
+    setNumItemsSelected((prev) => {
       if (prev + amount < 0) return 0;
       return prev + amount;
     });
+  };
+
+  const handleCartAddition = (amount) => {
+    setNumItemsInCart((prev) => {
+      return prev + amount;
+    });
+    setNumItemsSelected(0);
   };
   return (
     <div className={`${styles.actingBody} ${kumbhSans.className}`}>
@@ -127,14 +135,14 @@ export default function EcommerceProductPage() {
 
           <div>
             <div className={styles.addToCartControls}>
-              <button onClick={() => handleCartAddition(-1)}>
+              <button onClick={() => handleItemIncrement(-1)}>
                 <Image
                   src={IMAGES.minus}
                   alt="minus number of items from the cart"
                 />
               </button>
-              <div>{numItemsInCart}</div>
-              <button onClick={() => handleCartAddition(1)}>
+              <div>{numItemsSelected}</div>
+              <button onClick={() => handleItemIncrement(1)}>
                 <Image
                   src={IMAGES.plus}
                   alt="plus number of items to the cart"
@@ -146,10 +154,12 @@ export default function EcommerceProductPage() {
                 <path
                   d="M20.925 3.641H3.863L3.61.816A.896.896 0 0 0 2.717 0H.897a.896.896 0 1 0 0 1.792h1l1.031 11.483c.073.828.52 1.726 1.291 2.336C2.83 17.385 4.099 20 6.359 20c1.875 0 3.197-1.87 2.554-3.642h4.905c-.642 1.77.677 3.642 2.555 3.642a2.72 2.72 0 0 0 2.717-2.717 2.72 2.72 0 0 0-2.717-2.717H6.365c-.681 0-1.274-.41-1.53-1.009l14.321-.842a.896.896 0 0 0 .817-.677l1.821-7.283a.897.897 0 0 0-.87-1.114ZM6.358 18.208a.926.926 0 0 1 0-1.85.926.926 0 0 1 0 1.85Zm10.015 0a.926.926 0 0 1 0-1.85.926.926 0 0 1 0 1.85Zm2.021-7.243-13.8.81-.57-6.341h15.753l-1.383 5.53Z"
                   fill="#fff"
-                  fill-rule="nonzero"
+                  fillRule="nonzero"
                 />
               </svg>
-              <div>Add to cart</div>
+              <div onClick={() => handleCartAddition(numItemsSelected)}>
+                Add to cart
+              </div>
             </button>
           </div>
         </div>
