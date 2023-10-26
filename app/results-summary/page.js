13cfa435a716fea1f0773a70e5edd958e77659f4
daniel-clubb <danclubb004@gmail.com>
2023-10-26 15:49:04 +0100

    removed footer from results summary

--- app/results-summary/page.js
+++ app/results-summary/page.js
@@ -147,18 +147,6 @@ export default function ResultsSummary() {
           </div>
         </div>
       </main>
-
-      <div className="attribution">
-        Challenge by{" "}
-        <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
-          Frontend Mentor{" "}
-        </a>
-        | Coded by{" "}
-        <a href="https://danclubb.vercel.app" target="_blank">
-          Daniel Clubb
-        </a>
-        .
-      </div>
     </Fragment>
   );
 }
