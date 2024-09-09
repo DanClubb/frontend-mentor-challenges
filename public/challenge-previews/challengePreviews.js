5c0b50f141e6cba2472585db4fb9edb5291f22b4
daniel-clubb <danclubb004@gmail.com>
2024-09-09 22:56:28 +0100

    added preview photo to project card on homepage

--- public/challenge-previews/challengePreviews.js
+++ public/challenge-previews/challengePreviews.js
@@ -1,14 +1,16 @@
 const CHALLENGE_PREVIEWS = {
-  resultsSummary: require("./results-summary.jpg").default,
-  columnPreviewCard: require("./column-preview-card.jpg").default,
-  newsHomepage: require("./news-homepage.jpg").default,
-  productPreviewCard: require("./product-preview-card.jpg").default,
-  sunnysideAgencyLandingPage: require("./sunnyside-landing-page-preview.jpg")
-    .default,
-  loopstudiosLandingPage: require("./loopstudios-landing-page-preview.jpg")
-    .default,
-  huddleLandingPage: require("./huddle-landing-page-preview.jpg").default,
-  ecommerceProductPage: require("./ecommerce-product-page-preview.jpg").default,
+    resultsSummary: require("./results-summary.jpg").default,
+    socialLinks: require("./social-links.jpg").default,
+    columnPreviewCard: require("./column-preview-card.jpg").default,
+    newsHomepage: require("./news-homepage.jpg").default,
+    productPreviewCard: require("./product-preview-card.jpg").default,
+    sunnysideAgencyLandingPage: require("./sunnyside-landing-page-preview.jpg")
+        .default,
+    loopstudiosLandingPage: require("./loopstudios-landing-page-preview.jpg")
+        .default,
+    huddleLandingPage: require("./huddle-landing-page-preview.jpg").default,
+    ecommerceProductPage: require("./ecommerce-product-page-preview.jpg")
+        .default,
 };
 
 export default CHALLENGE_PREVIEWS;
