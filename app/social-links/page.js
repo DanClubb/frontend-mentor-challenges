7ede9b263d9199d4d4204bafba4bf9f7a92a1412
daniel-clubb <danclubb004@gmail.com>
2024-09-10 12:11:11 +0100

    made html more semantic and made each link wider

--- app/social-links/page.js
+++ app/social-links/page.js
@@ -10,43 +10,57 @@ export default function SocialLinks() {
                     <p className={styles.location}>London, Untied Kingdom</p>
                 </div>
 
-                <div className={styles.links}>
-                    <a
-                        href="https://github.com/danclubb"
-                        className={styles.link}
-                        target="_blank"
-                    >
-                        Github
-                    </a>
-                    <a
-                        href="https://www.linkedin.com/in/danielclubb/"
-                        className={styles.link}
-                        target="_blank"
-                    >
-                        LinkedIn
-                    </a>
-                    <a
-                        href="https://www.danclubb.com"
-                        className={styles.link}
-                        target="_blank"
-                    >
-                        Dan Clubb
-                    </a>
-                    <a
-                        href="https://www.frontendmentor.io/profile/DanClubb"
-                        className={styles.link}
-                        target="_blank"
-                    >
-                        Frontend Mentor
-                    </a>
-                    <a
-                        href="https://www.instagram.com/dan.clubb/"
-                        className={styles.link}
-                        target="_blank"
-                    >
-                        Instagram
-                    </a>
-                </div>
+                <ul className={styles.links}>
+                    <li>
+                        <a
+                            href="https://github.com/danclubb"
+                            className={styles.link}
+                            target="_blank"
+                        >
+                            Github
+                        </a>
+                    </li>
+
+                    <li>
+                        <a
+                            href="https://www.linkedin.com/in/danielclubb/"
+                            className={styles.link}
+                            target="_blank"
+                        >
+                            LinkedIn
+                        </a>
+                    </li>
+
+                    <li>
+                        <a
+                            href="https://www.danclubb.com"
+                            className={styles.link}
+                            target="_blank"
+                        >
+                            Dan Clubb
+                        </a>
+                    </li>
+
+                    <li>
+                        <a
+                            href="https://www.frontendmentor.io/profile/DanClubb"
+                            className={styles.link}
+                            target="_blank"
+                        >
+                            Frontend Mentor
+                        </a>
+                    </li>
+
+                    <li>
+                        <a
+                            href="https://www.instagram.com/dan.clubb/"
+                            className={styles.link}
+                            target="_blank"
+                        >
+                            Instagram
+                        </a>
+                    </li>
+                </ul>
             </div>
         </main>
     );
